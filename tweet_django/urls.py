48813ce768e6a69be943c19f202c99a92905df2f
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-10-04 23:27:29 +0300

    Added test page and static files

--- tweet_django/urls.py
+++ tweet_django/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from tweets.views import Index
+
 urlpatterns = [
+    url(r'^$', Index.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
